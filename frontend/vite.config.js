d0b76062c1ade7f837fd7fc2bc86717643c0622b
NelloreEswaraiahBharath <eswaraiahbharath@gmail.com>
2025-01-28 19:53:20 +0530

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: true,
+    host: "0.0.0.0",
     port: 4002,
     },
 })
